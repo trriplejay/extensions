e4a9c08beb51945d82e31e27592dfc7ba6f8d131
trriplejay <johns@jfrog.com>
2022-06-08 15:55:18 -0700

    test delete

--- steps/test/ApprovalGates/onExecute.sh
+++
@@ -1,7 +0,0 @@
-requiresApproval() {
-  local requiresApproval=$(find_step_configuration_value "requiresApproval")
-  echo "requiresApproval: $requiresApproval"
-  true
-}
-
-execute_command requiresApproval
